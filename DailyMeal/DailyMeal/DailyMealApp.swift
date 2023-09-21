43a31c1a506b50f2f6d53fef95d227ac9998991a
KeremMus <keremcancu42@gmail.com>
2023-09-21 18:39:37 +0300

    first commit to create repo

---
+++ DailyMeal/DailyMeal/DailyMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMealApp.swift
+//  DailyMeal
+//
+//  Created by Kerem Can Bakır on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DailyMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
